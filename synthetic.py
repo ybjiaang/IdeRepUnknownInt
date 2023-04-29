a121aadfd2a08c609cfa7b2ca178707ea380bb0d
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2023-04-29 15:45:48 -0500

    before writing evaluation code

--- synthetic.py
+++ synthetic.py
@@ -65,6 +65,101 @@ def identify_bipartie_graph(list_maximal_subsets, set_all_maximal_cliques, mode=
         
         raise ValueError("Bipartie graph cannot be identified assuming pure child")
 
+def identify_latent_graph(biparG, list_maximal_cliques):
+    nb_estimated_latents = len(biparG)
+    estimated_adj_matrix = np.zeros((nb_estimated_latents, nb_estimated_latents))
+    intervention_target_set = set()
+
+    # step 0: create mG list
+    mG = []
+    for maximal_clique in list_maximal_cliques:
+        mG_per_inter = []
+        for i in range(nb_estimated_latents):
+            for j in range(i+1, nb_estimated_latents):
+                union = set(biparG[i] + biparG[j])
+                if len(exist_superset(union, maximal_clique, proper=False)) == 0:
+                    mG_per_inter.append((i, j))
+
+        mG.append(mG_per_inter)
+
+    # step 1: remove any pairs that has appeared twice
+
+    count_matrix = np.zeros((nb_estimated_latents, nb_estimated_latents))
+    for mG_per_inter in mG:
+        for pair in mG_per_inter:
+            count_matrix[pair[0]][pair[1]] += 1
+            count_matrix[pair[1]][pair[0]] += 1
+
+    to_delete = []
+    for i in range(nb_estimated_latents): 
+        for j in range(i+1, nb_estimated_latents):
+            if count_matrix[i][j] > 1:
+                to_delete.append((i, j))
+
+    mG_deleted = []
+    for mG_per_inter in mG:
+        mG_deleted.append(list(x for x in mG_per_inter if x not in to_delete))
+
+    # step 2: Add edges for colliders
+    for mG_per_inter in mG_deleted:
+        if len(mG_per_inter) > 1:
+            try:
+                common_element = list(set(mG_per_inter[0]).intersection(set(mG_per_inter[1])))[0]
+            except:
+                raise ValueError("no common element")
+
+            intervention_target_set.add(common_element)
+            for v_tuple in mG_per_inter:
+                h_1 = v_tuple[0]
+                h_2 = v_tuple[1]
+
+                if h_1 == common_element:
+                    estimated_adj_matrix[h_1][h_2] = 1
+                else:
+                    estimated_adj_matrix[h_2][h_1] = 1
+            
+            mG_deleted.remove(mG_per_inter)
+
+    # step 3: Add compelled edges
+    newInv = True
+    while newInv:
+        newInv = False
+
+        for mG_per_inter in mG_deleted:
+            assert(len(mG_per_inter) <= 1)
+
+            if len(mG_per_inter) == 1:
+                v_tuple = mG_per_inter[0]
+                h_1 = v_tuple[0]
+                h_2 = v_tuple[1]        
+
+                if h_1 in intervention_target_set:
+                    estimated_adj_matrix[h_2][h_1] = 1
+                    intervention_target_set.add(h_2)
+                    newInv = True
+                    mG_deleted.remove(mG_per_inter)
+                    continue
+                if h_2 in intervention_target_set:
+                    estimated_adj_matrix[h_1][h_2] = 1
+                    intervention_target_set.add(h_1)
+                    newInv = True
+                    mG_deleted.remove(mG_per_inter)
+                    continue
+
+    # step 3: Add unoriented edges
+    for mG_per_inter in mG_deleted:
+        assert(len(mG_per_inter) <= 1)
+
+        if len(mG_per_inter) == 1:
+            v_tuple = mG_per_inter[0]
+            h_1 = v_tuple[0]
+            h_2 = v_tuple[1]
+
+            estimated_adj_matrix[h_2][h_1] = 1
+            estimated_adj_matrix[h_1][h_2] = 1
+
+    return estimated_adj_matrix
+
 class BipGraph():
     """
     The class that contains infomation about the bipartite causal graph between 
@@ -100,6 +195,7 @@ class BipGraph():
 
             # generate_weights (might be unstable to generate weights using gaussian. because some weights might be too small)
             # self.weights = np.abs(np.random.normal(size=self.weights.shape))
+            # 0.5 - 2
             self.weights = np.abs(np.random.uniform(low=0.25, high=1, size=self.weights.shape))
             self.weights *= self.adj
             self.weights /= np.linalg.norm(self.weights, axis=1, keepdims=True)
@@ -230,47 +326,16 @@ for candidate_set in list_valid_subsets:
 identify bipartite graph
 """
 biparG = identify_bipartie_graph(list_maximal_subsets, set_all_maximal_cliques)
+print("biparite graph")
 print(biparG)
 
 """
 identify latent graph
 """
+latentG = identify_latent_graph(biparG, list_maximal_cliques)
+print("latent graph")
+print(latentG)
 
-# step 0: create mG list
-mG = []
-nb_estimated_latents = len(biparG)
-for maximal_clique in list_maximal_cliques:
-    mG_per_inter = []
-    for i in range(nb_estimated_latents):
-        for j in range(i+1, nb_estimated_latents):
-            union = set(biparG[i] + biparG[j])
-            if len(exist_superset(union, maximal_clique, proper=False)) == 0:
-                mG_per_inter.append((i, j))
-
-    mG.append(mG_per_inter)
-
-# step 1: remove any pairs that has appeared twice
-
-count_matrix = np.zeros((nb_estimated_latents, nb_estimated_latents))
-for mG_per_inter in mG:
-    for pair in mG_per_inter:
-        count_matrix[pair[0]][pair[1]] += 1
-        count_matrix[pair[1]][pair[0]] += 1
-
-to_delete = []
-for i in range(nb_estimated_latents): 
-    for j in range(i+1, nb_estimated_latents):
-        if count_matrix[i][j] > 1:
-            to_delete.append((i, j))
-
-mG_deleted = []
-for mG_per_inter in mG:
-    mG_deleted.append(list(x for x in mG_per_inter if x not in to_delete))
-
-# step 2: Add edges for colliders
-for mG_per_inter in mG_deleted:
-    if len(mG_per_inter) > 1:
-        common_element = list(set(mG_per_inter[0]).intersection(set(mG_per_inter[1])))[0]
-        
-
-        
+"""
+evaluation
+"""
