ee5ebd59e78c46e5f43a55feab9353d611996caa
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2023-05-01 21:10:34 -0500

    add two python files

---
+++ graph.py
@@ -0,0 +1,184 @@
+import numpy as np
+import networkx as nx
+
+"""
+Notation for adjancy matrix
+If X_i -> X_j, then A[j][i] = 1. In other words, the ith row of the matrix is all the parents of X_i
+"""
+
+
+
+class BipGraph():
+    """
+    The class that contains infomation about the bipartite causal graph between 
+    observed and hidden variables
+    """
+    def __init__(self, num_hidden, num_observed, prob = 0.5, epsilon=1, graphtype='purechild'):
+        self.num_hidden = num_hidden
+        self.num_observed = num_observed
+        self.adj = np.zeros((self.num_observed, self.num_hidden))
+        self.weights = np.zeros_like(self.adj)
+        self.graphtype = graphtype
+        self.epsilon = epsilon
+        self.prob = prob
+
+        self.gen_random_graph()
+
+        # self.adj = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1], [1, 1, 1]])
+        print(self.adj)
+
+    def gen_random_graph(self, attempts = 1000):
+        if self.graphtype == 'purechild':
+            if self.num_hidden >= self.num_observed:
+                raise ValueError('num of hidden is not smaller than the num of observed')
+
+            # generate a random biparite graph first
+            self.adj = np.random.multinomial(1, [1/self.num_hidden]*self.num_hidden, size=self.num_observed)
+            self.adj = self.adj | np.random.binomial(1, self.prob, size = self.adj.shape)
+            for i in range(self.num_hidden):
+                for j in range(self.num_hidden):
+                    self.adj[i][j] = (i == j)
+
+            # randomize the columns
+            np.random.shuffle(np.transpose(self.adj))
+        
+        if self.graphtype == 'singlesource':
+            if self.num_hidden >= self.num_observed:
+                raise ValueError('num of hidden is not smaller than the num of observed')
+            
+            self.adj = np.random.multinomial(1, [1/self.num_hidden]*self.num_hidden, size=self.num_observed)
+            self.adj = self.adj | np.random.binomial(1, self.prob, size = self.adj.shape)
+
+        # generate_weights (might be unstable to generate weights using gaussian. because some weights might be too small)
+        # self.weights = np.abs(np.random.normal(size=self.weights.shape))
+        # 0.5 - 2
+        # self.weights = np.abs(np.random.uniform(low=0.5, high=2, size=self.weights.shape)) * np.random.choice([-1, 1], size=self.weights.shape, p=[1./2, 1./2])
+        self.weights = np.abs(np.random.uniform(low=0.5, high=2, size=self.weights.shape)) * np.random.choice([-1, 1], size=self.weights.shape, p=[1./2, 1./2])
+        self.weights *= self.adj
+        # self.weights /= np.linalg.norm(self.weights, axis=1, keepdims=True)
+        # print(self.weights)
+
+    def generate_samples(self, latents):
+        _, n = latents.shape
+
+        observed = self.weights @ latents + np.random.normal(0,self.epsilon, (self.num_observed, n))
+        
+        return observed
+
+class LatentDAG():
+    def __init__(self, num_hidden, edge_prob = 0.5, epsilon=1, graphtype='purechild'):
+        self.num_hidden = num_hidden
+        self.adj = np.zeros((self.num_hidden, self.num_hidden))
+        self.weights = np.zeros_like(self.adj)
+        self.edge_prob = edge_prob
+        self.graphtype = graphtype
+        self.epsilon = epsilon
+
+        self.get_random_latent_dag()
+        
+        # self.adj = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
+        # self.adj = np.array([[0, 0], [1, 0]])
+        print(self.adj)
+    
+    def get_random_latent_dag(self):
+        # nothing to be done here
+        if self.graphtype == 'purechild':
+            self.perm = np.arange(self.num_hidden)
+            np.random.shuffle(self.perm) # topsort
+            for i in range(self.num_hidden):
+                for j in range(i + 1, self.num_hidden):
+                    if np.random.binomial(1, p = self.edge_prob) == 1:
+                        self.adj[self.perm[j], self.perm[i]] = 1
+
+        if self.graphtype == 'singlesource':
+            self.perm = np.arange(self.num_hidden)
+            np.random.shuffle(self.perm) # topsort
+            for i in range(1, self.num_hidden):
+                for j in range(i + 1, self.num_hidden):
+                    if np.random.binomial(1, p = self.edge_prob) == 1:
+                        self.adj[self.perm[j], self.perm[i]] = 1
+                if sum(self.adj[self.perm[j]]) == 0:
+                    self.adj[self.perm[0], self.perm[j]] = 1
+            
+
+
+        # generate_weights
+        # only positive weights
+        # self.weights = np.abs(np.random.normal(size=self.weights.shape))
+        self.weights = np.random.uniform(low=0.5, high=2, size=self.weights.shape) * np.random.choice([-1, 1], size=self.weights.shape, p=[1./2, 1./2])
+        self.weights *= self.adj
+        # for i in range(self.num_hidden):
+        #     norm = np.linalg.norm(self.weights[i,:])
+        #     if norm > 0:
+        #         self.weights[i,:] /= norm
+
+        # print(self.weights)
+            
+    
+    def generate_samples(self, n, int_target):
+        latents = np.zeros((self.num_hidden, n))
+        for i in self.perm:
+            if i == int_target:
+                latent = np.random.normal(0,self.epsilon,n)
+            else:
+                latent = self.weights[i, :] @ latents + np.random.normal(0,self.epsilon,n)
+            latents[i,:] = latent
+        
+        return latents
+
+
+class Latent_and_Bipartite_graph():
+    """
+    Class that combines information about Latent and Bipartite causal graphs
+    """
+    def __init__(self, num_hidden, num_observed, epsilon = 0.1, latent_dag_density = 0.5, bip_graph_density = 0.6, mode='purechild'):
+        self.num_hidden = num_hidden
+        self.num_observed = num_observed
+
+        self.latentdag = LatentDAG(num_hidden, epsilon = epsilon, edge_prob = latent_dag_density, graphtype = mode)
+        self.bipgraph = BipGraph(num_hidden, num_observed, epsilon = epsilon, prob = bip_graph_density, graphtype = mode)
+
+    def generate_samples(self, n, int_target, puregraph = False):
+        # If int_target is -1, then obversational distributions, otherwise, it is one of the hidden variables
+
+        assert(-1 <= int_target < self.num_hidden)
+
+        if puregraph:
+
+            intervene_adj = np.copy(self.latentdag.adj)
+            if int_target >= 0:
+                intervene_adj[int_target, :] = 0
+
+            B_true = intervene_adj + np.identity(self.num_hidden).astype(int) 
+            B = B_true
+
+            for _ in range(self.num_hidden):
+                B = B_true @ B
+            
+            B = np.identity(self.num_hidden) @ B @ np.identity(self.num_hidden)
+            connecting_B = np.identity(self.num_hidden)
+            for i in range(self.num_hidden):
+                for j in range(i+1, self.num_hidden):
+                    row_i = B_true[i,:]
+                    row_j = B_true[j,:]
+
+                    connecting_B[i, j] = row_i @ B @ row_j + row_j @ B @ row_i
+                    connecting_B[j, i] = connecting_B[i, j]
+            # print("conneting B")
+            # print(connecting_B)
+            udj_cov = np.identity(self.num_observed)
+            for i in range(self.num_observed):
+                for j in range(i+1, self.num_observed):
+                    row_i = self.bipgraph.adj[i,:]
+                    row_j = self.bipgraph.adj[j,:]
+
+                    udj_cov[i, j] = row_i @ connecting_B @ row_j + row_j @ connecting_B @ row_i
+                    udj_cov[j, i] = udj_cov[i, j]
+
+            return (udj_cov > 0).astype(int)
+
+        else:
+            latents = self.latentdag.generate_samples(n, int_target)
+            observed = self.bipgraph.generate_samples(latents)
+
+            return observed
---
+++ metric.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+def count_accuracy(B_true, B_est):
+    """
+    Args:
+        B_true (np.ndarray): [d, d] ground truth graph, {0, 1}
+        B_est (np.ndarray): [d, d] estimate, {0, 1, -1}, -1 is undirected edge in CPDAG
+    # SHD = undirected extra (skeleton) + undirected missing (skeleton) + reverse (directed graph)
+    # unoriented_correct = # undirected edges in the cpdag that has a corresponding true edge in the true dag
+    """
+    d = len(B_true)
+    assert (len(B_est) == d)
+    undirected_extra = 0
+    undirected_missing = 0
+    reverse = 0
+    unoriented_correct = 0
+    total_edges = 0
+    for i in range(d):
+        for j in range(i + 1, d):
+            undir_true = (B_true[i][j] == 1 or B_true[j][i] == 1)
+            undir_est = (B_est[i][j] == 1 or B_est[i][j] == -1 or B_est[j][i] == 1 or B_est[j][i] == -1)
+            if undir_true:
+                total_edges += 1
+            if undir_true and (not undir_est):
+                undirected_missing += 1
+            elif (not undir_true) and undir_est:
+                undirected_extra += 1
+            elif undir_true and undir_est:
+                if B_est[i][j] == -1 or B_est[j][i] == -1:
+                    # Undirected edge in est
+                    unoriented_correct += 1
+                elif B_true[i][j] != B_est[i][j]:
+                    # Directed edge in est, but reversed
+                    reverse += 1
+    fdr = 0
+    if total_edges > 0:
+        fdr = reverse/total_edges
+    return {"shd": undirected_extra + undirected_missing + reverse,
+            "undirected_extra": undirected_extra,
+            "undirected_missing": undirected_missing,
+            "reverse": reverse,
+            "unoriented_correct": unoriented_correct,
+            "total_edges": total_edges,
+            "fdr": fdr}
+
+def get_metrics(MMgraph, latentG, biparG, mapping):
+    num_hidden, num_observed = MMgraph.num_hidden, MMgraph.num_observed
+    B_true = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+    B_bi_true = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+    B_latent_true = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+
+    for u in range(num_hidden):
+        for v in range(num_hidden):
+            if MMgraph.latentdag.adj[v, u] == 1:
+                B_true[v][u] = 1
+                B_latent_true[v][u] = 1
+
+    for u in range(num_hidden):
+        for v in range(num_observed):
+            if MMgraph.bipgraph.adj[v, u] == 1:
+                B_true[num_hidden + v][u] = 1
+                B_bi_true[num_hidden + v][u] = 1
+
+    B_est = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+    B_bi_est = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+    B_latent_est = [[0 for i in range(num_hidden + num_observed)] for j in range(num_hidden + num_observed)]
+    nb_estimated_hidden = len(biparG)
+    for u in range(nb_estimated_hidden):
+        for v in range(u + 1, nb_estimated_hidden):
+            if latentG[u][v] == 1 and latentG[v][u] == 1:
+                B_est[mapping[u]][mapping[v]] = -1
+                B_latent_est[mapping[u]][mapping[v]] = -1
+            elif latentG[u][v] == 1:
+                B_est[mapping[u]][mapping[v]] = 1
+                B_latent_est[mapping[u]][mapping[v]] = 1
+            elif latentG[v][u] == 1:
+                B_est[mapping[v]][mapping[u]] = 1
+                B_latent_est[mapping[v]][mapping[u]] = 1
+
+    for u in range(nb_estimated_hidden):
+        for v in range(num_observed):
+            if v in biparG[u]:
+                B_est[num_hidden + v][mapping[u]] = 1
+                B_bi_est[num_hidden + v][mapping[u]] = 1
+
+    full_metrics = count_accuracy(B_true, B_est)
+    bi_metrics = count_accuracy(B_bi_true, B_bi_est)
+    latent_metrics = count_accuracy(B_latent_true, B_latent_est)
+    return (full_metrics, bi_metrics, latent_metrics)
+            
